47b1a1d9e9d602e63cd20f8eecb68951de21d9bc
ckat609 <armando@armandotello.com>
2023-08-19 21:15:53 -0500

    presskey working

--- 999_test/999_test.ino
+++ 999_test/999_test.ino
@@ -9,11 +9,11 @@ uint8_t buf[8] = {0}; // Keyboard report buffer
 #define LCDPIN_D6 7
 #define LCDPIN_D7 8
 
-#define PIN_W 47       // Pin for w
-#define PIN_A 45       // Pin for a
-#define PIN_S 43       // Pin for s
-#define PIN_D 41       // Pin for d
-#define PIN_PRG_CHG 39 // Pin for d
+#define BUTTON_11 47  // Pin for w
+#define BUTTON_12 45  // Pin for a
+#define BUTTON_13 43  // Pin for s
+#define BUTTON_14 41  // Pin for d
+#define BUTTON_PRG 39 // Pin for d
 
 LiquidCrystal lcd(LCDPIN_RS,
                   LCDPIN_EN,
@@ -22,40 +22,53 @@ LiquidCrystal lcd(LCDPIN_RS,
                   LCDPIN_D6,
                   LCDPIN_D7);
 
+const int MAXKEYS = 4;
+typedef struct
+{
+    char *title;
+    int keycodes[MAXKEYS];
+    char keys[MAXKEYS];
+    int pins[MAXKEYS];
+} program;
+
+program programs[3] = {{"SCULPT MODE", {4, 22, 7, 9}, {'A', 'S', 'D', 'F'}},
+                       {"TEXTURE PAINT", {30, 31, 32, 33}, {'1', '2', '3', '4'}},
+                       {"GREASE PENCIL", {20, 26, 8, 21}, {'Q', 'W', 'E', 'R'}}};
+
 char lcdTitle[] = "PROGRAM:";
-char *programs[3] = {"SCULPT MODE", "TEXTURE PAINT", "GREASE PENCIL"};
+// char *programs[3] = {"SCULPT MODE", "TEXTURE PAINT", "GREASE PENCIL"};
 int totalPrograms = (sizeof(programs) / sizeof(programs[0]));
 int currentProgram = 0;
 
-int prevStatePIN_W;
-int prevStatePIN_A;
-int prevStatePIN_S;
-int prevStatePIN_D;
-int prevStatePIN_PRG_CHG;
+int prevStateBUTTON_11;
+int prevStateBUTTON_12;
+int prevStateBUTTON_13;
+int prevStateBUTTON_14;
+int prevStateBUTTON_PRG;
 
-int currStatePIN_W;
-int currStatePIN_A;
-int currStatePIN_S;
-int currStatePIN_D;
-int currStatePIN_PRG_CHG;
+int currStateBUTTON_11;
+int currStateBUTTON_12;
+int currStateBUTTON_13;
+int currStateBUTTON_14;
+int currStateBUTTON_PRG;
 
 void setup()
 {
-    Serial.begin(115200); // Setup Serial communication
+    Serial.begin(9600); // Setup Serial communication
 
     lcd.begin(16, 2);
     // Set pinmode of Input pins
-    pinMode(PIN_W, INPUT_PULLUP);
-    pinMode(PIN_A, INPUT_PULLUP);
-    pinMode(PIN_S, INPUT_PULLUP);
-    pinMode(PIN_D, INPUT_PULLUP);
-    pinMode(PIN_PRG_CHG, INPUT_PULLUP);
+    pinMode(BUTTON_11, INPUT_PULLUP);
+    pinMode(BUTTON_12, INPUT_PULLUP);
+    pinMode(BUTTON_13, INPUT_PULLUP);
+    pinMode(BUTTON_14, INPUT_PULLUP);
+    pinMode(BUTTON_PRG, INPUT_PULLUP);
 
-    digitalWrite(PIN_W, HIGH);
-    digitalWrite(PIN_A, HIGH);
-    digitalWrite(PIN_S, HIGH);
-    digitalWrite(PIN_D, HIGH);
-    digitalWrite(PIN_PRG_CHG, HIGH);
+    digitalWrite(BUTTON_11, HIGH);
+    digitalWrite(BUTTON_12, HIGH);
+    digitalWrite(BUTTON_13, HIGH);
+    digitalWrite(BUTTON_14, HIGH);
+    digitalWrite(BUTTON_PRG, HIGH);
 }
 
 void loop()
@@ -63,63 +76,13 @@ void loop()
     lcd.setCursor(0, 0);
     lcd.print(lcdTitle);
     lcd.setCursor(0, 1);
-    lcd.print(programs[currentProgram]);
+    lcd.print(programs[currentProgram].title);
 
-    // When program change button is pressed
-    // currStatePIN_PRG_CHG = digitalRead(PIN_PRG_CHG);
-    // if (currStatePIN_PRG_CHG == LOW && prevStatePIN_PRG_CHG == HIGH)
-    // {
-    //     currentProgram = currentProgram < totalPrograms - 1 ? currentProgram + 1 : 0;
-    //     lcd.clear();
-    //     Serial.println(programs[currentProgram]);
-    // }
-    // prevStatePIN_PRG_CHG = currStatePIN_PRG_CHG;
-
-    changeProgram(programs, currentProgram, totalPrograms, currStatePIN_PRG_CHG, prevStatePIN_PRG_CHG, lcd);
-
-    // When button representing W is pressed
-    currStatePIN_W = digitalRead(PIN_W);
-    if (currStatePIN_W == LOW && prevStatePIN_W == HIGH)
-    {
-        buf[2] = 26;          // W keycode
-        Serial.write(buf, 8); // Send keypress
-        Serial.println("W");
-        releaseKey();
-    }
-    prevStatePIN_W = currStatePIN_W;
-
-    // When button representing A is pressed
-    currStatePIN_A = digitalRead(PIN_A);
-    if (currStatePIN_A == LOW && prevStatePIN_A == HIGH)
-    {
-        buf[2] = 4;           // A keycode
-        Serial.write(buf, 8); // Send keypress
-        Serial.println("A");
-        releaseKey();
-    }
-    prevStatePIN_A = currStatePIN_A;
-
-    // When button representing S is pressed
-    currStatePIN_S = digitalRead(PIN_S);
-    if (currStatePIN_S == LOW && prevStatePIN_S == HIGH)
-    {
-        buf[2] = 22;          // S keycode
-        Serial.write(buf, 8); // Send keypress
-        Serial.println("S");
-        releaseKey();
-    }
-    prevStatePIN_S = currStatePIN_S;
-
-    // When button representing D is pressed
-    currStatePIN_D = digitalRead(PIN_D);
-    if (currStatePIN_D == LOW && prevStatePIN_D == HIGH)
-    {
-        buf[2] = 7;           // D keycode
-        Serial.write(buf, 8); // Send keypress
-        Serial.println("D");
-        releaseKey();
-    }
-    prevStatePIN_D = currStatePIN_D;
+    changeProgram(currStateBUTTON_PRG, prevStateBUTTON_PRG, programs, currentProgram, totalPrograms, lcd);
+    pressKey(BUTTON_11, 0, currStateBUTTON_11, prevStateBUTTON_11, programs, currentProgram);
+    pressKey(BUTTON_12, 1, currStateBUTTON_12, prevStateBUTTON_12, programs, currentProgram);
+    pressKey(BUTTON_13, 2, currStateBUTTON_13, prevStateBUTTON_13, programs, currentProgram);
+    pressKey(BUTTON_14, 3, currStateBUTTON_14, prevStateBUTTON_14, programs, currentProgram);
 }
 // Function for Key Release
 void releaseKey()
@@ -129,14 +92,27 @@ void releaseKey()
     Serial.write(buf, 8); // Send Release key
 }
 
-void changeProgram(char *progs[], int &currProg, int &totalProgs, int &currStatePin, int &prevStatePin, LiquidCrystal &display)
+void pressKey(int pin, int currKey, int &currStatePin, int &prevStatePin, program progs[], int currProg)
 {
-    currStatePin = digitalRead(PIN_PRG_CHG);
+    currStatePin = digitalRead(pin);
+    if (currStatePin == LOW && prevStatePin == HIGH)
+    {
+        buf[2] = progs[currentProgram].keycodes[currKey]; // keycode
+        Serial.write(buf, 8);                             // Send keypress
+        releaseKey();
+        // Serial.println(progs[currentProgram].key[currKey]);
+    }
+    prevStatePin = currStatePin;
+}
+
+void changeProgram(int &currStatePin, int &prevStatePin, program progs[], int &currProg, int &totalProgs, LiquidCrystal &display)
+{
+    currStatePin = digitalRead(BUTTON_PRG);
     if (currStatePin == LOW && prevStatePin == HIGH)
     {
         currProg = currProg < totalProgs - 1 ? currProg + 1 : 0;
         display.clear();
-        Serial.println(progs[currProg]);
+        // Serial.println(progs[currProg].title); // Print the selected program
     }
     prevStatePin = currStatePin;
 }
